127d09c92ff9b7fdd4d95d2423bb1b9bb7ea1d4a
tagasouichiro <stitow14@gmail.com>
2016-11-20 00:56:41 +0900

    initial commit

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+# coding:utf-8
+
+def main():
+	print("Hello World")
+	message = "Oi! {}".format("test")
+	print(message)
+	name = raw_input('What is your name?\n')
+	print('Hi {}'.format(name))
+
+if __name__ == "__main__":
+	main()
+
