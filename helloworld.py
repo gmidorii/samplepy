84e1b95b771abfb89e2aedbc0fdc9b835d76efbd
tagasouichiro <stitow14@gmail.com>
2016-11-20 22:57:02 +0900

    add for loop

--- helloworld.py
+++ helloworld.py
@@ -1,11 +1,24 @@
 # coding:utf-8
 
+def greet(name):
+	print(name)
+
 def main():
 	print("Hello World")
 	message = "Oi! {}".format("test")
 	print(message)
-	name = raw_input('What is your name?\n')
-	print('Hi {}'.format(name))
+
+	# input
+	#name = raw_input('What is your name?\n')
+	#print('Hi {}'.format(name))
+
+	# for loop (plus iterator)
+	friends = ['john', 'Bob', 'Herey']
+	for it, name in enumerate(friends):
+		print("it {} : name{}".format(it, name))
+
+	# function
+	greet("Taga")
 
 if __name__ == "__main__":
 	main()
