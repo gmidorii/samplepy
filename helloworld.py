2e03b925685de1bbcfce8a499f924037161d45e7
tagasouichiro <stitow14@gmail.com>
2016-11-22 23:55:31 +0900

    add loop etc..

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,7 @@
 # coding:utf-8
 
+import sys
+
 def greet(name):
 	print(name)
 
@@ -7,6 +9,7 @@ def main():
 	print("Hello World")
 	message = "Oi! {}".format("test")
 	print(message)
+	print("\n")
 
 	# input
 	#name = raw_input('What is your name?\n')
@@ -16,9 +19,28 @@ def main():
 	friends = ['john', 'Bob', 'Herey']
 	for it, name in enumerate(friends):
 		print("it {} : name{}".format(it, name))
+	print("\n")
 
 	# function
 	greet("Taga")
+	print("\n")
+
+	# string join
+	print('JOIN' + ' string')
+	print("\n")
+
+	# array + loop
+	array = {'apple': 1, 'banana': 3}
+	sum_array = sum(array[key] for key in array)
+	print(sum_array)
+	print("\n")
+
+	# arg + try-except
+	try:
+		#arg = sys.argv[1:]
+		#print(arg)
+	except ValueError:
+		print("Error")
 
 if __name__ == "__main__":
 	main()
